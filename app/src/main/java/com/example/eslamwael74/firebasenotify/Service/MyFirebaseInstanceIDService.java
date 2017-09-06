8be53613a2a7e29cf3eaac865ec9c02268abb20a
Eslam Wael <e.wael@4artstudio.net>
2017-09-06 23:54:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/eslamwael74/firebasenotify/Service/MyFirebaseInstanceIDService.java
@@ -0,0 +1,51 @@
+package com.example.eslamwael74.firebasenotify.Service;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.example.eslamwael74.firebasenotify.app.Config;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Eslam Wael on 7/31/2017.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService{
+
+
+    private static final String TAG = MyFirebaseInstanceIDService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        // Saving reg id to shared preferences
+        storeRegIdInPref(refreshedToken);
+
+        // sending reg id to your server
+        sendRegistrationToServer(refreshedToken);
+        Log.d("Firebase", "token "+ FirebaseInstanceId.getInstance().getToken());
+
+
+        // Notify UI that registration has completed, so the progress indicator can be hidden.
+        Intent registrationComplete = new Intent(Config.REGISTRATION_COMPLETE);
+        registrationComplete.putExtra("token", refreshedToken);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+    }
+
+    private void sendRegistrationToServer(final String token) {
+        // sending gcm token to server
+        Log.e(TAG, "sendRegistrationToServer: " + token);
+    }
+
+    private void storeRegIdInPref(String token) {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(Config.SHARED_PREF, 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("regId", token);
+        editor.commit();
+    }
+}
